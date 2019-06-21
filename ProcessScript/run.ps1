27d922356d3d0be8404d52b7797dd60c27f83be4
Tyler Leonhardt <tylerl0706@gmail.com>
2019-06-21 15:36:44 -0700

    Add module and settings support and settings to UI

--- ProcessScript/run.ps1
+++ ProcessScript/run.ps1
@@ -3,18 +3,43 @@ using namespace System.Net
 # Input bindings are passed in via param block.
 param($Request, $TriggerMetadata)
 
-# Write to the Azure Functions log stream.
-Write-Host "PowerShell HTTP trigger function processed a request."
+$settings = if($Request.Body.Settings) {
+    $Request.Body.Settings
+} else {
+    "PSGallery"
+}
 
 # Interact with query parameters or the body of the request.
 if($Request.Body.script) {
+    Write-Verbose "Script specified." -Verbose
     try {
-        $results = @(Invoke-ScriptAnalyzer -ScriptDefinition $Request.Body.script)
+        $results = @(Invoke-ScriptAnalyzer -ScriptDefinition $Request.Body.script -Settings $settings)
         $status = [HttpStatusCode]::OK
     } catch {
         $status = [HttpStatusCode]::BadRequest
         $results = $_
     }
+} elseif($Request.Body.ModuleName) {
+    Write-Verbose "Module name specified." -Verbose
+    $tmpDir = [System.IO.Path]::GetTempPath()
+
+    try {
+        Write-Verbose "Attempting to save module..." -Verbose
+        Save-Module $Request.Body.ModuleName -Path $tmpDir -ErrorAction Stop
+        Write-Verbose "Saved module..." -Verbose
+        
+        Write-Verbose "Attempting to invoke PSScriptAnalyzer..." -Verbose
+        $results = @(Invoke-ScriptAnalyzer -Path (Join-Path $tmpDir $Request.Body.ModuleName) -Recurse -Settings $settings)
+        Write-Verbose "Invoked PSScriptAnalyzer..." -Verbose
+
+        $status = [HttpStatusCode]::OK
+    } catch {
+        $status = [HttpStatusCode]::BadRequest
+        $results = $_
+    } finally {
+        Remove-Item -Recurse -Force -ErrorAction SilentlyContinue $tmpDir
+    }
+
 } else {
     $status = [HttpStatusCode]::BadRequest
     $results = "No script specified in the body."
